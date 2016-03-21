90dc250f0be5be3438006d5e1daed6d501c19fc8
Zhibarev <ggb097@gmail.com>
2016-03-21 16:22:22 +0300

    add 1.1
    add 1.1

---
+++ 2/1/List/list.h
@@ -0,0 +1,16 @@
+#pragma once
+
+using namespace std;
+
+class List
+{
+public:
+    virtual void add(int newValue, int position) = 0;
+    virtual int remove(int position) = 0;
+    virtual int extract(int position) = 0;
+    virtual void print() = 0;
+    virtual bool empty() = 0;
+    virtual int length() = 0;
+    virtual int search(int value) = 0;
+    virtual ~List(){}
+};
