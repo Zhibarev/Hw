babe2a103109a2ef03d007c378c87e17ad8edd3b
Zhibarev <ggb097@gmail.com>
2016-05-19 21:56:39 +0300

    fix bugs
    fix bugs and unite some classes

--- 2/5/1/treenode.cpp
+++ 2/5/1/treenode.cpp
@@ -1,9 +1,64 @@
 #include "treenode.h"
-#include "operand.h"
-#include "operator.h"
 
-TreeNode::TreeNode(const std::string &string, int index)
+TreeNode::TreeNode(const std::string &expression, int &i)
 {
-    if (string[index] <= '9' && string[index] >= '0')
+    if (expression[i] <= '9' && expression[i] >= '0')
+    {
+        isNumber = true;
+        while (expression[i] <= '9' && expression[i] >= '0')
+        {
+         value = value * 10 + expression[i] - '0';
+          i++;
+        }
+        i++;
+    }
+    else
+    {
+        value = expression[i + 1];
+        i += 3;
+        left = new TreeNode(expression, i);
+        right = new TreeNode(expression, i);
+        i++;
+    }
+}
 
+int TreeNode::calculate() const
+{
+    if (!isNumber)
+        switch (value)
+        {
+        case '+':
+            return left->calculate() + right->calculate();
+        case '-':
+            return left->calculate() - right->calculate();
+        case '*':
+            return left->calculate() * right->calculate();
+        case '/':
+            return left->calculate() / right->calculate();
+        }
+    else
+        return value;
+}
+
+void TreeNode::print(std::ofstream &out) const
+{
+    if (!isNumber)
+    {
+        out << "(" << (char) value << " ";
+        left->print(out);
+        out << " ";
+        right->print(out);
+        out << ")";
+    }
+    else
+        out << value;
+}
+
+TreeNode::~TreeNode()
+{
+    if (!isNumber)
+    {
+        delete left;
+        delete right;
+    }
 }
---
+++ 2/5/2/set.h
@@ -0,0 +1,8 @@
+#pragma once
+
+template <typedef T>
+class Set<T>
+{
+public:
+    Set();
+};
