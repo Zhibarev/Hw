bd63b28f8ebe03da931c7c699c4e7de518e24330
Zhibarev Egor <ggb097@gmail.com>
2017-03-18 21:14:14 +0300

    fix

--- 4/2/1.hs
+++ 4/2/1.hs
@@ -1,4 +1,4 @@
-reverseList :: Num a => [a] -> [a]
+reverseList :: [a] -> [a]
 reverseList list = reverseList' list [] where
 	reverseList' [] result = result
 	reverseList' list result = reverseList' (tail list) (head list : result)
