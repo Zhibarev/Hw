6c30b538755cdb91dd0b7ef9ff9745127e39fc86
Zhibarev <ggb097@gmail.com>
2017-03-18 18:34:00 +0300

    add 1.2

---
+++ 4/1/2.hs
@@ -0,0 +1,9 @@
+fibonacci :: Int -> Int
+fibonacci n | n >= 0 = fibonacciRecursive n 0 1
+			| otherwise = error "Argument < 0"
+
+fibonacciRecursive :: Int -> Int -> Int -> Int
+fibonacciRecursive n current next | n == 0 = current
+                                  | otherwise = fibonacciRecursive (n - 1) next (current + next)
+
+
