861dde5969ed330175cca5cb921344e95af33cc1
Zhibarev Egor <ggb097@gmail.com>
2017-03-18 23:42:36 +0300

    fix conc

--- 4/2/5.hs
+++ 4/2/5.hs
@@ -1,6 +1,6 @@
 sumOfThreeLists :: [Int] -> [Int] -> [Int] -> [Int]
 sumOfThreeLists [] [] [] = []
-sumOfThreeLists first second third = [head' first + head' second + head' third] ++ sumOfThreeLists (tail' first) (tail' second) (tail' third) where
+sumOfThreeLists first second third = (head' first + head' second + head' third) : sumOfThreeLists (tail' first) (tail' second) (tail' third) where
 	head' [] = 0
 	head' list = head list
 	tail' [] = []
