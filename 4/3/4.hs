7ab1f8382ffe6976aa9072fd65916427d12e0030
Zhibarev <ggb097@gmail.com>
2017-03-22 21:58:57 +0300

    remove extra ()

--- 4/3/4.hs
+++ 4/3/4.hs
@@ -1,12 +1,12 @@
 import Data.List
 
 checkStr :: [Char] -> Bool
-checkStr str = check' str ("&") where -- "&" != [] for don't describe (check' str [])
+checkStr str = check' str "&" where -- "&" != [] for don't describe (check' str [])
 	check' [] "&" = True
 	check' [] _ = False
 	check' (x:xs) (b:bs) 
 		| elem x "[{(" = check' xs (x:b:bs)
-		| x == ']'     = if b == '[' then (check' xs bs) else False
-		| x == ')'     = if b == '(' then (check' xs bs) else False
-		| x == '}'     = if b == '{' then (check' xs bs) else False
+		| x == ']'     = if b == '[' then check' xs bs else False
+		| x == ')'     = if b == '(' then check' xs bs else False
+		| x == '}'     = if b == '{' then check' xs bs else False
 		| otherwise    = check' xs (b:bs)
