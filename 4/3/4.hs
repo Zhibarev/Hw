1e58c7298487d17dd2b6f2d908eb551af13906e4
Zhibarev <ggb097@gmail.com>
2017-03-22 21:57:05 +0300

    fix

--- 4/3/4.hs
+++ 4/3/4.hs
@@ -1,11 +1,12 @@
 import Data.List
 
-checkString str :: [Char] -> Bool
-checkString str = check' str brackets where
-	check' [] [] = True
+checkStr :: [Char] -> Bool
+checkStr str = check' str ("&") where -- "&" != [] for don't describe (check' str [])
+	check' [] "&" = True
 	check' [] _ = False
-	check' (x:xs) $ b:bs | elem x "[{(" = check' xs $ x:b:bs
-	                     | x == "]"     = b == "[" then check' xs bs else False
-						 | x == ")"     = b == "(" then check' xs bs else False
-						 | x == "}"     = b == "{" then check' xs bs else False
-						 | otherwise    = check' xs $ b:bs
+	check' (x:xs) (b:bs) 
+		| elem x "[{(" = check' xs (x:b:bs)
+		| x == ']'     = if b == '[' then (check' xs bs) else False
+		| x == ')'     = if b == '(' then (check' xs bs) else False
+		| x == '}'     = if b == '{' then (check' xs bs) else False
+		| otherwise    = check' xs (b:bs)
