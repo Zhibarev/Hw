e9c476b5e3d22340cf2fe6da3bb2041f02e17d59
Zhibarev <ggb097@gmail.com>
2017-03-22 20:39:25 +0300

    add type

--- 4/3/3.hs
+++ 4/3/3.hs
@@ -1,4 +1,5 @@
 import Data.List
 import Data.Ord
 
+maxNearby :: [Int] -> Int
 maxNearby l = snd $ maximumBy (comparing fst) (reverse $ zip (map (uncurry (+)) $ zip (0:init l) $ tail l ++ [0]) [1..])
