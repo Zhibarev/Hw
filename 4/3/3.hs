b6164c269c2026e3083d5e8e1f5a89d4cf6bb270
Zhibarev Egor <ggb097@gmail.com>
2017-03-24 10:32:50 +0300

    rewrite

--- 4/3/3.hs
+++ 4/3/3.hs
@@ -1,5 +1,7 @@
-import Data.List
-import Data.Ord
-
 maxNearby :: [Int] -> Int
-maxNearby l = snd $ maximumBy (comparing fst) (reverse $ zip (map (uncurry (+)) $ zip (0:init l) $ tail l ++ [0]) [1..])
+maxNearby l = maxNearby (0 : l) 1 0 0 where
+	maxNearby (_ : []) _ maxPos _ = maxPos
+	maxNearby (x : y : []) pos maxPos max = if x > max then pos else maxPos
+	maxNearby (x : y : z : zs) pos maxPos max = if (x + z) > max 
+		then maxNearby (y : z : zs) (pos + 1) pos $ x + z
+		else maxNearby (y : z : zs) (pos + 1) maxPos max
