97b680c8e2f15f497d6139dae77ac788a075fff8
Zhibarev Egor <ggb097@gmail.com>
2019-03-18 19:24:23 +0300

    small changes

--- 4/3/5.hs
+++ 4/3/5.hs
@@ -1,5 +1,5 @@
 fib :: Int -> Int
 
-fib n = (signum n) * (fib' 0 1 n) where
+fib n = fib' 0 1 n where
         fib' x _ 0 = x
-        fib' curr next n = (signum n) * (fib' next (curr + next) $ n - (signum n))
+        fib' curr next n = fib' next (curr + next * (signum n)) $ n - (signum n)
